68718f4102ed89475948825a28529e45380684ba
WallaHassan <wallahassan93.wh@gmail.com>
2023-09-22 16:23:02 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,10 @@
 global    main
-xtern    printf
+          extern    printf
 main:
-mov   edi, format
-xor   eax, eax
-call  printf
-mov 	eax, 0
-ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
+
